7b2e3c2e8eb12355d320cbbc3b80ac01843682a9
Fikata12 <filipkarabashev@gmail.com>
2023-04-04 02:42:34 +0300

    Create controllers

--- RiddleMaker.Web/Controllers/AddController.cs
+++ RiddleMaker.Web/Controllers/AddController.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using RiddleMaker.Services.Data;
+using RiddleMaker.Web.ViewModels;
+
 namespace RiddleMaker.Web.Controllers
 {
     public class AddController : Controller
     {
+        readonly IRiddlesService riddlesService;
+
+        public AddController(IRiddlesService riddlesService)
+        {
+            this.riddlesService = riddlesService;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRiddleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await riddlesService.AddAsync(model);
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- RiddleMaker.Web/Controllers/GetController.cs
+++ RiddleMaker.Web/Controllers/GetController.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RiddleMaker.Common;
+using RiddleMaker.Services.Data;
+using RiddleMaker.Web.ViewModels;
 
 namespace RiddleMaker.Web.Controllers
 {
     public class GetController : Controller
     {
-        [HttpGet]
-        public IActionResult Get()
+        readonly IRiddlesService riddlesService;
+
+        public GetController(IRiddlesService riddlesService)
         {
-            return View();
+            this.riddlesService = riddlesService;
         }
-    }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return View(await riddlesService.GetRandomRiddleAsync());
+        }
+
+        public async Task<IActionResult> CheckAnswer(GetRiddleViewModel model)
+        {
+
+            if (await riddlesService.CheckAnswer(model.Id, model.UserAnswer))
+            {
+                model.IsCorrect = true;
+                model.Message = Messages.CorrectAnswerMessage;
+            }
+            else
+            {
+				model.IsCorrect = false;
+				model.Message = Messages.WrongAnswerMessage;
+			}
+			return View("Get", model);
+		}
+	}
 }
