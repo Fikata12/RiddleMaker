d42fcb6bc8e4a436172ecaa91db8c055896c9770
Fikata12 <filipkarabashev@gmail.com>
2023-04-03 19:22:08 +0300

    Create view model for add

---
+++ RiddleMaker.Web.ViewModels/AddViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RiddleMaker.Common.EntityConfiguration;
+using System.ComponentModel.DataAnnotations;
+
+namespace RiddleMaker.Web.ViewModels
+{
+    public class AddViewModel
+    {
+        [MaxLength(ValidationConstants.RiddleLength)]
+        public string Riddle { get; set; } = null!;
+
+        [MaxLength(ValidationConstants.AnswerLength)]
+        public string Answer { get; set; } = null!;
+    }
+}
