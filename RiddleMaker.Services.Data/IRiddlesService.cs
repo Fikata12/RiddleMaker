c2f4b5b9202d02429cb7b263b25cf05d7108ad38
Fikata12 <filipkarabashev@gmail.com>
2023-04-04 02:40:26 +0300

    Add interface for riddles service

--- RiddleMaker.Services.Data/IRiddlesService.cs
+++ RiddleMaker.Services.Data/IRiddlesService.cs
@@ -1,6 +1,15 @@
-﻿namespace RiddleMaker.Services.Data
+﻿using RiddleMaker.Data.Models;
+using RiddleMaker.Web.ViewModels;
+
+namespace RiddleMaker.Services.Data
 {
     public interface IRiddlesService
     {
-    }
+        Task AddAsync(AddRiddleViewModel model);
+
+        Task<GetRiddleViewModel?> GetRandomRiddleAsync();
+
+        Task<bool> CheckAnswer(int riddleId, string answer);
+
+	}
 }
