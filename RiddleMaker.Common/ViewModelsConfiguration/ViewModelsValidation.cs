be7c3fe0d02785fab50dbd7d1826eaabbd542dfa
Fikata12 <filipkarabashev@gmail.com>
2023-04-04 02:39:18 +0300

    Add view models validation class

---
+++ RiddleMaker.Common/ViewModelsConfiguration/ViewModelsValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace RiddleMaker.Common.ViewModelsConfiguration
+{
+    public static class ViewModelsValidation
+    {
+        //Riddle
+        public const int RiddleLength = 217;
+
+        //Answer
+        public const int AnswerLength = 31;
+    }
+}
