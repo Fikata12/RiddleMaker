239b18d45e748ff89d593541ad847da053c6ec94
Fikata12 <filipkarabashev@gmail.com>
2023-04-04 02:40:51 +0300

    Add riddles service

--- RiddleMaker.Services.Data/RiddlesService.cs
+++ RiddleMaker.Services.Data/RiddlesService.cs
@@ -1,9 +1,52 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RiddleMaker.Data;
+using RiddleMaker.Data.Models;
+using RiddleMaker.Web.ViewModels;
 
 namespace RiddleMaker.Services.Data
 {
-    public class RiddlesService
+    public class RiddlesService : IRiddlesService
     {
+        readonly IMapper mapper;
+        readonly RiddleMakerContext context;
 
+        public RiddlesService(IMapper mapper, RiddleMakerContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        public async Task AddAsync(AddRiddleViewModel model)
+        {
+            Riddle riddle = mapper.Map<Riddle>(model);
+            await context.AddAsync(riddle);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<GetRiddleViewModel?> GetRandomRiddleAsync()
+        {
+            if (await context.Riddles.CountAsync() == 0)
+            {
+                return null;
+            }
+
+            Random rnd = new Random();
+            int index = rnd.Next(1, await context.Riddles.CountAsync() + 1);
+
+            Riddle? riddle = await context.Riddles.FindAsync(index);
+            while (riddle == null)
+            {
+                index = rnd.Next(1, await context.Riddles.CountAsync());
+                riddle = await context.Riddles.FindAsync(index);
+            }
+
+            return mapper.Map<GetRiddleViewModel>(riddle);
+        }
+
+        public async Task<bool> CheckAnswer(int riddleId, string answer)
+        {
+            return (await context.Riddles.FindAsync(riddleId))!.Text.ToLower() == answer.ToLower();
+        }
     }
 }
